bb5de230e3cb6af66bebfb211e6dcf315f430f1d
ishaan vyas <ishaan47isop@gmail.com>
2024-11-22 21:46:12 +0530

    Update egg snatcher.py

--- egg snatcher.py
+++ egg snatcher.py
@@ -2,19 +2,19 @@ import random
 def eggsnatcher():
     score=0
 animals=["python","fox","monkey"]
-rounds=5 #number of rounds to play
+rounds=5 
 score=0
 
-print("welcome to the amazing game made by Ishaan Vyas, this game is called egg snatcher in this there are 3 sneaky animals and in each round you have to guess who has stolen the egg now play the game and enjoy!!!!")
+print("welcome to the amazing game made by Ishaan Vyas, this game is called egg snatcher in this there are 3 sneaky animals and in each round you have to guess who has stolen the egg, so what are you waiting for now play the game and enjoy!!!!")
+
 for i in range(1,rounds+1):
-    #randomly select the animal having egg
     animals_with_egg = random.choice(animals)
     print(" round{}: choose an animal youre suspicious of from (python,fox,monkey):".format(i))
     guess=input("your guess:")
 
     if guess not in animals:
         print("Wow, you are DUMB, even a lkg kid could guess from the list of THREE above")
-        continue #skip this round
+        continue
     if guess == animals_with_egg:
         print("great! now you can make an delicious egg fry to eat and you can even eat",format(animals_with_egg),"if you want to")
         score += 1
@@ -22,29 +22,33 @@ for i in range(1,rounds+1):
     else:
         print("wrong! now you cant make an egg fry and ",format(animals_with_egg),"(who stole the egg) also can come AGAIN")
 print(score)
-print("Game over! your score is.",score," [there is an easter egg too in which you can get a mystery egg on getting all 5 rounds correct and one on getting 3 rounds correct]")
-#check for BABY GRONK EGG
+
 if score == 5:
-    print("congratulations! you have earned the BABY GRONK EGG, now you are a true SKIBBIDI")
+    print("congratulations! you have earned the SIGMA EGG, now you are a true GEN Z")
 else:
     print("play again for another mystery egg")
-#check for THUNDER EGG
-if score =< 3:
+    
+if score == 3:
     print("congratulations! you have earned the THUNDER EGG, now you can party with THOR and LOKI")
 
-#check for CURSED EGG
-if score == 0:
+elif score == 0:
     print("oh noo! you have earned the CURSED EGG, if you want to break the curse replay the game")
 
-#check for FROOTI "by meenu maam"
-if score =< 1:
-    print("great! now finally youll get a FROOTI from meenu maam")
+elif score == 1:
+    print("great! now you'll finally get a FROOTI from meenu maam")
 
+elif score == 2:
+    print("lucky you! you just earned MONEY EGG, you have a money luck for 24 hours $$$$$$$$$$$")
+
+elif score == 4:
+    print("nice job! you got MEMORY EGG, it will help you increase your memory")
+
+print("it also have more of the egg achievements, if you want to collect all of achievements play again and have fun!!") 
 print("thank you for playing our game please write a comment for us to how to improve on this")
 x=(input("comment:"))
 print("thank you")
     
-if "_name_"=="_main_":
+if "name"=="main":
     eggsnatcher()
      
 
